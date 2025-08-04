308487dfe316f27a975860ddd082e1a27d883cc6
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-08-04 14:24:03 -0300

    Incluindo o Filter e o toLocaleString

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-
 const list = document.querySelector("ul"); // Lista do products.js
 const buttonShowAll = document.querySelector(".show-all"); // ForEach --> mostrar tudo
 const buttonShowDiscont = document.querySelector(".show-discont"); // Map --> para dar 10% de desconto
-const buttonshowAmount = document.querySelector(".show-amount"); // Reduce --> soma os valores
+const buttonShowAmount = document.querySelector(".show-amount"); // Reduce --> soma os valores
+const buttonOnlyVegans = document.querySelector(".only-vegans"); // Filter --> vai mostar só os lanches veganos
+
+// serve para colocar o valor em formato real (0,00)
+function formatCurrency(value) {
+  const newValue = value.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  return newValue
+}
+
 // serve para mostrar tudo
 function showAll(productsArray) {
   let myLi = "";
@@ -12,14 +23,14 @@ function showAll(productsArray) {
     <li>
         <img src=${product.src}>
         <p>${product.name}</p>
-        <p class="price">R$ ${product.price.toFixed(2)}</p>
+        <p class="price">R$ ${formatCurrency(product.price)}</p>
       </li>
     `;
   });
   list.innerHTML = myLi;
 
   // Some com o reduce quando mostrar outros itens
-  document.querySelector("#summary").innerHTML = '';
+  document.querySelector("#summary").innerHTML = "";
 }
 
 // server para da os 10% de desconto
@@ -34,13 +45,12 @@ function showDiscont() {
   showAll(newPrices);
 
   // Some com o map também
-document.querySelector("#summary").innerHTML = '';
-
+  document.querySelector("#summary").innerHTML = "";
 }
 
 // somar todos os valores dos lanches
 function showAmount() {
-list.innerHTML = '';
+  list.innerHTML = "";
 
   const total = menuOptions.reduce((acumulador, item) => {
     return acumulador + item.price;
@@ -57,33 +67,33 @@ list.innerHTML = '';
     return !item.vegan ? acumulador + item.price : acumulador;
   }, 0);
 
-  const summary = document.querySelector("#summary")
+  const summary = document.querySelector("#summary");
 
   summary.innerHTML = `
   <li>
-  <p>Valor total dos lanches <span style="color: #ffd100"> R$ ${total.toFixed(
-    2
-  )}</span></p>
+  <p>Valor total dos lanches <span style="color: #ffd100"> R$ ${formatCurrency(total)}</span></p>
   </li>
   <li>
-   <p>Valor total dos lanches com 10% de desconto <span style="color: #ffd100"> R$ ${totalDiscont.toFixed(
-     2
-   )}</span></p>
+   <p>Valor total dos lanches com 10% de desconto <span style="color: #ffd100"> R$ ${totalDiscont}</span></p>
   </li>
   <li>
-<p>Valor total lanches Veganos <span style="color: #ffd100"> R$ ${totalVegans.toFixed(
-    2
-  )}</span></p>
+<p>Valor total lanches Veganos <span style="color: #ffd100"> R$ ${totalVegans}</span></p>
   </li>
   <li>
-<p>Valor total lanches com Carne <span style="color: #ffd100"> R$ ${totalMeats.toFixed(
-    2
-  )}</span></p>
+<p>Valor total lanches com Carne <span style="color: #ffd100"> R$ ${totalMeats}</span></p>
   </li>
   `;
+}
+
+// Mastra apenas os lanches veganos
+function showOnlyVegans() {
+  const filterOnlyVegans = menuOptions.filter((burger) => burger.vegan);
+
+  showAll(filterOnlyVegans);
 }
 
 // Chama os eventos
 buttonShowAll.addEventListener("click", () => showAll(menuOptions)); // botão forEch mostrar tudo
 buttonShowDiscont.addEventListener("click", showDiscont); // botão map desconto
-buttonshowAmount.addEventListener("click", showAmount); // botão reduce somar todos os valores
+buttonShowAmount.addEventListener("click", showAmount); // botão reduce somar todos os valores
+buttonOnlyVegans.addEventListener("click", showOnlyVegans); // botão filtar mostra os veganos
